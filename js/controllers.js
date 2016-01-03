9609bda894fd6dd699f54ad83ed4f64aebdb1fb3
srini88 <pvsrinivas.ece@gmail.com>
2016-01-03 01:39:50 -0600

    added details partial - when the user clicks on any artist - it goes to the details page

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,4 @@
 
-
-
 var artistControllers = angular.module("artistControllers",[]);
 artistControllers.controller("ListController", function myController ($scope, $http){
 	//using http service - handle communication between http server and a client
@@ -11,3 +9,18 @@ artistControllers.controller("ListController", function myController ($scope, $h
 		$scope.direction = '';  //by default ascending
 	});
 });
+
+
+// in this controller we need to get the route parameters the user clicked on
+
+artistControllers.controller("DetailsController", function myController ($scope, $http, $routeParams){
+	//using http service - handle communication between http server and a client
+	$http.get("js/data.json")
+	.then(function(response){
+		$scope.artists = response.data;
+		$scope.whichItem = $routeParams.itemId;
+	});
+});
+
+
+
