fe463923f6afa21e43c5c86d44b87940e1bd594a
srini88 <pvsrinivas.ece@gmail.com>
2016-01-02 22:03:53 -0600

    small app with controller - just printing firstname and last name

---
+++ js/controllers.js
@@ -0,0 +1,7 @@
+var myApp = angular.module("myApp",[]);
+myApp.controller("myController", function myController ($scope){
+
+	$scope.firstName = "Srini",
+	$scope.jobTitle = "front end developer",
+	$scope.skills = "Angular guru"
+});
